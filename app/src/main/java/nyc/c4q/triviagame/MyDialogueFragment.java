24fe0d954ec6d9115128b008d31347108f7707e5
Wayne Kellman <waynekellman@ac.c4q.nyc>
2018-01-15 16:48:15 -0500

    randomized question

--- app/src/main/java/nyc/c4q/triviagame/MyDialogueFragment.java
+++ app/src/main/java/nyc/c4q/triviagame/MyDialogueFragment.java
@@ -11,10 +11,15 @@ import android.view.ViewGroup;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
 import nyc.c4q.triviagame.model.Question;
 
 
-public class MyDialogueFragment extends DialogFragment {
+public class MyDialogueFragment extends DialogFragment implements View.OnClickListener {
 
 
     private static final String BUNDLE_KEY = "Question";
@@ -43,57 +48,71 @@ public class MyDialogueFragment extends DialogFragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-    View v = inflater.inflate(R.layout.fragment_main, container, false);
+        View v = inflater.inflate(R.layout.fragment_main, container, false);
 
         TextView answer1TV = v.findViewById(R.id.answer1_itemview);
         TextView answer2TV = v.findViewById(R.id.answer2_itemview);
         TextView answer3TV = v.findViewById(R.id.answer3_itemview);
         TextView answer4TV = v.findViewById(R.id.answer4_itemview);
         TextView questionTV = v.findViewById(R.id.question_itemview);
+        String[] randomizeAnswerArray = new String[4];
 
-        answer1TV.setText("1. " + question.getIncorrect_answers().get(0));
-        answer2TV.setText("2. " + question.getIncorrect_answers().get(1));
-        answer3TV.setText("3. " + question.getIncorrect_answers().get(2));
-        answer4TV.setText("4. " + question.getCorrect_answer());
+        List<String> randomizedQuestions = new ArrayList<>();
+        randomizedQuestions.add(question.getIncorrect_answers().get(0));
+        randomizedQuestions.add(question.getIncorrect_answers().get(1));
+        randomizedQuestions.add(question.getIncorrect_answers().get(2));
+        randomizedQuestions.add(question.getCorrect_answer());
+        long seed = System.nanoTime();
+        Collections.shuffle(randomizedQuestions, new Random(seed));
+
+
+        answer1TV.setText("1. " + randomizedQuestions.get(0));
+        answer2TV.setText("2. " + randomizedQuestions.get(1));
+        answer3TV.setText("3. " + randomizedQuestions.get(2));
+        answer4TV.setText("4. " + randomizedQuestions.get(3));
         questionTV.setText(question.getQuestion());
+        answer1TV.setOnClickListener(this);
+        answer2TV.setOnClickListener(this);
+        answer3TV.setOnClickListener(this);
+        answer4TV.setOnClickListener(this);
 
-        answer1TV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "You could do better! Wrong Answer!", Toast.LENGTH_LONG).show();
-                dismiss();
-            }
-        });
-
-        answer2TV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "You could do better! Wrong Answer!", Toast.LENGTH_LONG).show();
-                dismiss();
-
-            }
-        });
-
-        answer3TV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "You could do better! Wrong Answer!", Toast.LENGTH_LONG).show();
-                dismiss();
-
-            }
-        });
-
-        answer4TV.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Toast.makeText(getActivity(), "Congratulation! This is a Correct Answer!", Toast.LENGTH_LONG).show();
-                score += 1;
-//                passing score back to Activity
-                scoreListener.passScore(score);
-                dismiss();
-
-            }
-        });
+//        answer1TV.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(), "You could do better! Wrong Answer!", Toast.LENGTH_LONG).show();
+//                dismiss();
+//            }
+//        });
+//
+//        answer2TV.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(), "You could do better! Wrong Answer!", Toast.LENGTH_LONG).show();
+//                dismiss();
+//
+//            }
+//        });
+//
+//        answer3TV.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(), "You could do better! Wrong Answer!", Toast.LENGTH_LONG).show();
+//                dismiss();
+//
+//            }
+//        });
+//
+//        answer4TV.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getActivity(), "Congratulation! This is a Correct Answer!", Toast.LENGTH_LONG).show();
+//                score += 1;
+////                passing score back to Activity
+//                scoreListener.passScore(score);
+//                dismiss();
+//
+//            }
+//        });
         return v;
     }
 
@@ -102,4 +121,19 @@ public class MyDialogueFragment extends DialogFragment {
     }
 
 
+    @Override
+    public void onClick(View view) {
+        if (view instanceof TextView) {
+            if (((TextView) view).getText().toString().contains(question.getCorrect_answer())) {
+                Toast.makeText(getActivity(), "Congratulation! This is a Correct Answer!", Toast.LENGTH_LONG).show();
+                score += 1;
+//                passing score back to Activity
+                scoreListener.passScore(score);
+                dismiss();
+            } else {
+                Toast.makeText(getActivity(), "You could do better! Wrong Answer!", Toast.LENGTH_LONG).show();
+                dismiss();
+            }
+        }
+    }
 }
